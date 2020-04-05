274cefc30bafe0b4b7df5c2fb43ef2f961869961
rtelles64 <rtelles64@gmail.com>
2020-04-04 17:57:10 -0700

    Update loading sql queries

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -14,32 +14,32 @@ class SqlQueries:
         State table insert query
     """
     imm_dem_table_insert = ("""
-        SELECT d.City, d.State, d.`State Code`, i.`Travel Mode`,
-               i.Gender, d.`Foreign-born`, d.Race, i.`Visa Type`
-        FROM imm_data i
-        JOIN dem_data d ON d.`State Code` = i.`Dest State`
+        SELECT d.city, d.state, d.state_code, i.travel_mode,
+               i.gender, d.foreign_born, d.race, i.visa_type
+        FROM staging_imm i
+        JOIN staging_dem d ON d.state_code = i.dest_state
     """)
 
     city_table_insert = ("""
-        SELECT DISTINCT City, State, `State Code`, Race
-        FROM dem_data
-        ORDER BY State
+        SELECT DISTINCT city, state, state_code, race
+        FROM staging_dem
+        ORDER BY state
     """)
 
     entry_port_table_insert = ("""
-        SELECT DISTINCT `Entry Port`,
-               CAST(`Travel Mode` AS INT), `Dest State`, `Visa Type`
-        FROM imm_data
-        WHERE `Dest State` != "null"
+        SELECT DISTINCT entry_port,
+               CAST(travel_mode AS INT), dest_state, visa_type
+        FROM staging_imm
+        WHERE dest_state != "null"
         ORDER BY 1
     """)
 
     state_table_insert = ("""
-        SELECT DISTINCT State,
-               CAST(SUM(`Male Population`) AS INT) AS `Male Pop Total`,
-               CAST(SUM(`Female Population`) AS INT) AS `Fem Pop Total`,
-               CAST(SUM(`Foreign-born`) AS INT) AS `Foreign-born Tot`
-        FROM dem_data
+        SELECT DISTINCT state,
+               CAST(SUM(male_pop) AS INT),
+               CAST(SUM(female_pop) AS INT),
+               CAST(SUM(foriegn_born) AS INT)
+        FROM staging_dem
         GROUP BY 1
-        ORDER BY State, `Foreign-born tot` DESC
+        ORDER BY 1, 4 DESC
     """)
