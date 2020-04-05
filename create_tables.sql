24e1e587433673dd71577572c3330a2a103b892d
rtelles64 <rtelles64@gmail.com>
2020-04-04 17:53:20 -0700

    Change staging imm travel_mode type

--- create_tables.sql
+++ create_tables.sql
@@ -47,7 +47,7 @@ CREATE TABLE public.staging_dem (
 CREATE TABLE public.staging_imm (
 	imm_id int NOT NULL AUTO_INCREMENT,
 	entry_port varchar(256),
-	travel_mode int,
+	travel_mode numeric,
 	dest_state varchar(256),
 	birth_year int,
 	gender varchar(5),
